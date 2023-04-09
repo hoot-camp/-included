8e5cbea51d25c35e7fdadd7f70bbdf1d13a16dc8
isaac-110 <isaac-110@hi16.ca>
2023-04-09 07:56:01 +0000

    fix: identifiers

--- itemTypes.ts
+++ itemTypes.ts
@@ -1,7 +1,7 @@
-import type { Id } from 'go.vote/App/@id/itemTypes'
+import type { $Id } from 'go.vote/App/@$id/itemTypes'
 
 export const set$Item = 'set$Item'
 export type $Item = {
     $item: number
 }
-export type Set$Item = Id & $Item
+export type Set$Item = $Id & $Item
